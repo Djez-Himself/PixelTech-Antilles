6bae056d1265b27d18e35b5fb219ecb1c628c55a
Djez-Himself <djez.himself@gmail.com>
2025-11-29 14:58:18 -0400

    Correction syntaxe template literals GoogleAnalytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -6,18 +6,18 @@ export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        src={\https://www.googletagmanager.com/gtag/js?id=\\}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
-        {\
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '\', {
+          gtag('config', '${GA_MEASUREMENT_ID}', {
             page_path: window.location.pathname,
           });
-        \}
+        `}
       </Script>
     </>
   )
